e54b5ff2c17876eb28eb9be7254827fdfc33e77a
xiaoming0000 <1925952439@qq.com>
2026-06-25 15:06:14 +0800

    fix: 格式代码

--- .prettierrc.ts
+++ .prettierrc.ts
@@ -1,10 +1,14 @@
-module.exports = {
+import { type Config } from 'prettier';
+
+const config: Config = {
 	singleQuote: true,
 	trailingComma: 'all',
 	endOfLine: 'lf',
-	printWidth: 180,
+	printWidth: 150,
 	tabWidth: 4,
 	useTabs: true,
 	quoteProps: 'preserve',
 	bracketSpacing: true,
 };
+
+export default config;
